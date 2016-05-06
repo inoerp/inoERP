83e4b946900331d37aed9563fb74c9d3befad0f8
Nishit <contact@inoideas.org>
2016-05-06 22:23:08 +0800

    0.5.1

--- inoerp/modules/sys/calendar/calendar_template.php
+++ inoerp/modules/sys/calendar/calendar_template.php
@@ -27,7 +27,24 @@
    </ul>
    <div class="tabContainer"> 
     <div id="tabsLine-1" class="tabContent">
-
+     <table class="table table-hover table-sm cal cal-month">
+      <thead>
+       <tr class="cal-row cal-header">
+        <th class="cal-th col2">Date :  <i class="cal-date"></i></th>
+       </tr>
+      </thead>
+      <tbody>
+       <?php
+       $c_count = 1;
+       for ($i = 23; $i > 0; $i--) {
+        echo "<tr class='cal-row'>";
+        echo "<td class='clickable cell_{$i}'>$c_count </td>";
+        echo "</tr>";
+        $c_count++;
+       }
+       ?>
+      </tbody>
+     </table>
     </div> 
     <div id="tabsLine-2" class="tabContent">
      <table class="table table-hover table-sm cal cal-week">
@@ -49,19 +66,43 @@
        for ($i = 24; $i > 0; $i--) {
         $rowclass = ($i % 2 == 0 ) ? 'cal-major' : 'cal-minor';
         echo "<tr class='cal-row $rowclass'>";
-         for($j=0 ; $j <=7 ; $j ++ ){
-          $tdval = ($j == 0 && $rowclass == 'cal-major') ? $i/2 . ' AM ' : ' ';
-          echo "<td class='clickable cell_{$i}_{$j}'> $tdval </td>";
-         }
+        for ($j = 0; $j <= 7; $j ++) {
+         $tdval = ($j == 0 && $rowclass == 'cal-major') ? $i / 2 . ' AM ' : ' ';
+         echo "<td class='clickable cell_{$i}_{$j}'> $tdval </td>";
+        }
         echo "</tr>";
        }
        ?>
       </tbody>
      </table>
-
     </div> 
     <div id="tabsLine-3" class="tabContent">
-
+     <table class="table table-hover table-sm cal cal-month">
+      <thead>
+       <tr class="cal-row cal-header">
+        <th class="cal-th col2">Mon <i class="cal-date"></i></th>
+        <th class="cal-th col3">Tue <i class="cal-date"></i></th>
+        <th class="cal-th col4">Wed <i class="cal-date"></i></th>
+        <th class="cal-th col5">Thu <i class="cal-date"></i></th>
+        <th class="cal-th col6">Fri <i class="cal-date"></i></th>
+        <th class="cal-th col7">Sat <i class="cal-date"></i></th>
+        <th class="cal-th col8">Sun <i class="cal-date"></i></th>
+       </tr>
+      </thead>
+      <tbody>
+       <?php
+       $c_count = 0;
+       for ($i = 7; $i > 0; $i--) {
+        echo "<tr class='cal-row height-100'>";
+        for ($j = 0; $j <= 6; $j ++) {
+         $c_count++;
+         echo "<td class='clickable cell_{$i}_{$j}'>$c_count </td>";
+        }
+        echo "</tr>";
+       }
+       ?>
+      </tbody>
+     </table>
     </div> 
    </div>
   </div>
