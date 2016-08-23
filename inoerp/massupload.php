385030f88841b6f3239098e0d5b4d0c0136f78cd
Nishit <contact@inoideas.org>
2016-08-23 12:35:37 +0800

    0.5.1

--- inoerp/massupload.php
+++ inoerp/massupload.php
@@ -17,16 +17,16 @@ if (!empty($_GET['class_name'])) {
  $search_result_statement .= '<th> Link </th>';
  $search_result_statement .='</tr></thead>';
  If (!empty($all_search_paths)) {
-	$search_result_statement .= '<tbody>';
-	foreach ($all_search_paths as $record) {
-	 $search_result_statement .='<tr>';
-	 $search_result_statement .='<td>' . $record->name . '</td>';
-	 $search_result_statement .='<td>' . $record->description . '</td>';
-	 $search_result_statement .='<td>' . $record->module_code . '</td>';
-	 $search_result_statement .='<td><a href="' . HOME_URL . $record->path_link . '">' . HOME_URL . $record->path_link . '</a></td>';
-	 $search_result_statement .='</tr>';
-	}
-	$search_result_statement .='</tbody>';
+  $search_result_statement .= '<tbody>';
+  foreach ($all_search_paths as $record) {
+   $search_result_statement .='<tr>';
+   $search_result_statement .='<td>' . $record->name . '</td>';
+   $search_result_statement .='<td>' . $record->description . '</td>';
+   $search_result_statement .='<td>' . $record->module_code . '</td>';
+   $search_result_statement .='<td><a href="' . HOME_URL . $record->path_link . '">' . HOME_URL . $record->path_link . '</a></td>';
+   $search_result_statement .='</tr>';
+  }
+  $search_result_statement .='</tbody>';
  }
  $search_result_statement .='</table>';
 //	 echo '<pre>';
@@ -35,47 +35,61 @@ if (!empty($_GET['class_name'])) {
  return;
 }
 ?>
-<?php 
+<?php
 if (!empty($class_names)) {
  $class = $class_names;
  include_once("includes/functions/loader.inc");
-  if (empty($access_level) || ($access_level < 3 )) {
-	access_denied();
-	return;
+ if (empty($access_level) || ($access_level < 3 )) {
+  access_denied();
+  return;
  }
  $$class = new $class;
  $data_headers = $$class->field_a;
  $ignored_fields = ['created_by', 'creation_date', 'last_update_by', 'last_update_date'];
  $few_records = $class::find_few(5);
  $dataArray = [];
- foreach ($few_records as $rows) {
-	$datarow = [];
-	foreach ($data_headers as $columns) {
-	 if (!in_array($columns, $ignored_fields)) {
-		$datarow[$columns] = $rows->$columns;
-	 }
-	}
-	array_push($dataArray, $datarow);
+
+ if (!empty($few_records)) {
+  foreach ($few_records as $rows) {
+   $datarow = [];
+   foreach ($data_headers as $columns) {
+    if (!in_array($columns, $ignored_fields)) {
+     $datarow[$columns] = $rows->$columns;
+    }
+   }
+   array_push($dataArray, $datarow);
+  }
+ } else {
+  $data_headers = get_dbColumns($class);
+  if(empty($data_headers)){
+   die('No Database Structure Found. Check the class /table name');
+  }
+  $datarow = [];
+  foreach ($data_headers as $k => $columns) {
+   if (!in_array($columns, $ignored_fields)) {
+    $datarow[$columns] = $rows->$columns;
+   }
+  }
+  array_push($dataArray, $datarow);
  }
+
 
  $dl = new downloads();
  $dl->setProperty('_downloaded_data', $dataArray);
- 
- 
+
+
  if ($continue) {
- if (!empty($$class) && property_exists($$class, 'mass_upload_template_path')) {
-  $template_file_names = $class::$mass_upload_template_path;
- } else if (!empty($$class)) {
-  $template_file_names = ['extensions/file/massupload_template.php'];
- }
+  if (!empty($$class) && property_exists($$class, 'mass_upload_template_path')) {
+   $template_file_names = $class::$mass_upload_template_path;
+  } else if (!empty($$class)) {
+   $template_file_names = ['extensions/file/massupload_template.php'];
+  }
   include_once(THEME_DIR . '/main_template.inc');
-} else {
- $continue = false;
- echo "<h2>Could n't call the header</h2>";
- return;
-}
- 
- 
+ } else {
+  $continue = false;
+  echo "<h2>Could n't call the header</h2>";
+  return;
+ }
 }
 ?>
 <script type="text/javascript">
