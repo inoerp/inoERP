255989bd0cddec71e260e0d3638daac5b8e2f404
Nishit <contact@inoideas.org>
2016-09-02 17:50:48 +0800

    0.5.1

--- inoerp/extensions/comment/json.comment.php
+++ inoerp/extensions/comment/json.comment.php
@@ -2,7 +2,7 @@
   if(!empty($_GET['delete']) && $_GET['delete']==1 ){
    $comment_id = $_GET['extn_comment_id'];
 	 include_once 'comment.php?extn_commentt_id='.$comment_id;
-   $result=comment::delete($comment_id);
+   $result=extn_comment::delete($comment_id);
   
 	 echo '<div id="delete_comment">';
  if ($result == 1) {
