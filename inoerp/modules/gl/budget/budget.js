8d1d7fae3ba849874700cb903d27b63898c7331a
inoerp <contact@inoideas.org>
2016-10-08 15:41:27 +0100

    0.6.1

--- inoerp/modules/gl/budget/budget.js
+++ inoerp/modules/gl/budget/budget.js
@@ -1,5 +1,2 @@
 $(document).ready(function () {
-$('#content').off('change', '#qa_cp_header_id') .on('change', '#qa_cp_header_id' , function(){
-$('a.show').trigger('click');
-})
  });
--- inoerp/modules/gl/budget/budget_template.php
+++ inoerp/modules/gl/budget/budget_template.php
@@ -1,99 +1,82 @@
-<form  method="post" id="gl_budget"  name="gl_budget">
- <span class="heading"><?php echo gettext('Quality Results') ?></span>
- <div id ="form_header">
-  <div id="tabsHeader">
-   <ul class="tabMain">
-    <li><a href="#tabsHeader-1"><?php echo gettext('Basic') ?></a></li>
-   </ul>
-   <div class="tabContainer"> 
-    <div id="tabsHeader-1" class="tabContent">
-     <div class="large_shadow_box"> 
-      <ul class="column header_field">
-       <li><?php
-        echo $f->l_select_field_from_object('qa_cp_header_id', $qa_cp_header_obj, 'qa_cp_header_id', 'plan_name', $$class->qa_cp_header_id, 'qa_cp_header_id', 'action', 1);
-        ?>
+<!-- * 
+inoERP
+ *
+ * @copyright   2014 Nishit R. Das
+ * @license     https://www.mozilla.org/MPL/2.0/
+ * @link        http://inoideas.org
+ * @source code https://github.com/inoerp/inoERP
+-->
+
+
+<div id ="form_header">
+ <form method="post" id="gl_budget"  name="gl_budget">
+  <span class="heading"><?php echo gettext('GL Budget') ?></span>
+  <div id ="form_header">
+   <div id="tabsHeader">
+    <ul class="tabMain">
+     <li><a href="#tabsHeader-1"><?php echo gettext('Basic Info') ?></a></li>
+     <li><a href="#tabsHeader-2"><?php echo gettext('Note') ?></a></li>
+     <li><a href="#tabsHeader-3"><?php echo gettext('Actions') ?></a></li>
+    </ul>
+    <div class="tabContainer"> 
+     <div id="tabsHeader-1" class="tabContent">
+      <ul class="column header_field two_column_form"> 
+       <li><?php $f->l_text_field_dr_withSearch('gl_budget_id') ?>
+        <a name="show" href="form.php?class_name=gl_budget&<?php echo "mode=$mode"; ?>" class="show document_id gl_budget_id"><i class="fa fa-refresh"></i></a> 
        </li>
-       <li><?php $f->l_text_field('reference_type', $reference_type); ?></li>
-       <li><?php $f->l_text_field('reference_entity', $reference_entity); ?></li>
-       <li><?php $f->l_text_field('reference_key_name', $ref_key_name, '' ,'' ,$always_readonly2, '' , $readonly2); ?></li>
-       <li><?php $f->l_text_field('reference_key_value', $ref_key_value, '' ,'' ,$always_readonly2, '' , $readonly2); ?></li>
-       <a name="show" href="form.php?class_name=gl_budget&<?php echo "mode=$mode"; ?>" class="show action document_id gl_budget ino-left-30-n"><i class="fa fa-refresh"></i></a>
+       <li><?php $f->l_text_field_dm('budget_name'); ?> 					</li>
+			 <li><?php $f->l_select_field_from_object('ledger_id', gl_ledger::find_all(), 'gl_ledger_id', 'ledger', $$class->ledger_id, 'ledger_id', '', '', $readonly1, 1); ?>             </li>
+			 <li><?php $f->l_select_field_from_array('status', gl_budget::$status_a, $$class->status, 'status', '', 1); ?>         </li> 
+			 <li><?php $f->l_date_fieldAnyDay_r('created_on', $$class->created_on, 'always_readonly') ?></li>
+			 <li><?php $f->l_date_fieldAnyDay_r('frozen_date', $$class->created_on, 'always_readonly') ?></li>
+			 <li><label><?php echo gettext('First Period') ?></label><?php
+					 if (!empty($period_name_stmt)) {
+						echo $period_name_stmt;
+					 } else {
+						$f->text_field_d('first_period_id');
+					 }
+					 ?>
+			 </li>
+			 <li><label><?php echo gettext('Current Period') ?></label><?php
+					 if (!empty($period_name_stmt)) {
+						echo $period_name_stmt;
+					 } else {
+						$f->text_field_d('first_period_id');
+					 }
+					 ?>
+			 </li>
+
+       <li><?php $f->l_text_field_d('description'); ?> 					</li>
+			 <li><label><?php echo gettext('Freeze Budget') ; ?></label><?php echo $f->checkBox_field('freeze_cb', ''); ?> 					</li>
       </ul>
+     </div>
+
+     <div id="tabsHeader-2" class="tabContent">
+      <div> <?php echo ino_attachement($file) ?> </div>
+     </div>
+     <div id="tabsHeader-3" class="tabContent">
+      <div> 
+       <div id="comments">
+        <div id="comment_list">
+						<?php echo!(empty($comments)) ? $comments : ""; ?>
+        </div>
+        <div id ="display_comment_form">
+						<?php
+						$reference_table = 'gl_budget';
+						$reference_id = $$class->gl_budget_id;
+						?>
+        </div>
+        <div id="new_comment">
+        </div>
+       </div>
+      </div>
      </div>
     </div>
+
    </div>
   </div>
- </div>
-
- <div id ="form_line" class="form_line"><span class="heading"><?php echo gettext('Quality Element Values') ?></span>
-  <div id="tabsLine">
-   <ul class="tabMain">
-    <li><a href="#tabsLine-1"><?php echo gettext('Basics') ?></a></li>
-   </ul>
-   <div class="tabContainer"> 
-    <div id="tabsLine-1" class="tabContent">
-     <div class="first_rowset"> 
-      <ul class="column header_field two_column_form"> 
-       <?php
-//        pa($qa_cp_element_obj);
-       foreach ($qa_cp_element_obj as $qa_cp_element) {
-        if (empty($qa_cp_element->sys_element_name)) {
-         continue;
-        }
-        $ce_table = 'qa_ce_' . $qa_cp_element->sys_element_name;
-        $qa_cp_element_value = null;
-         $field_value_obj = gl_budget::find_by_fieldName_referenceDetails($qa_cp_element->sys_element_name, $ref_key_name, $ref_key_value);
-         if ($field_value_obj) {
-          $field_value_key = $ce_table . '_value';
-          $qa_cp_element_value = $field_value_obj->$field_value_key;
-         } 
-
-        $label = !empty($qa_cp_element->element_name) ? $qa_cp_element->element_name : $qa_cp_element->sys_element_name;
-        echo "<li><label>$label </label>";
-        switch ($qa_cp_element->data_type) {
-         case 'LIST':
-          $qa_ce_lines = qa_ce_line::find_by_parent_id($qa_cp_element->qa_ce_header_id);
-          if (!empty($qa_ce_lines)) {
-           echo $f->select_field_from_object($qa_cp_element->sys_element_name, $qa_ce_lines, 'qa_ce_line_id', 'element_value', $qa_cp_element_value);
-          } else {
-           echo $f->text_field($qa_cp_element->sys_element_name, $qa_cp_element_value);
-          }
-          break;
-
-         case 'BOOLEAN' :
-          echo $f->checkBox_field($qa_cp_element->sys_element_name, $qa_cp_element_value);
-          break;
-
-         case 'FILE' :
-          $qa_cp_element_file_name = $qa_cp_element->sys_element_name . '_file';
-          $file_routing = extn_file::find_by_fieldName_referenceTable_and_id($qa_cp_element->sys_element_name, 'wip_wo_routing_line', $routing_line->wip_wo_routing_line_id);
-          echo ino_attachement($file_routing, $qa_cp_element_file_name);
-          echo $f->hidden_field($qa_cp_element->sys_element_name, $qa_cp_element_value);
-          break;
-
-         case 'OPTION_LIST' :
-          if (!empty($qa_cp_element->list_value_option_type)) {
-           echo $f->select_field_from_object($qa_cp_element->sys_element_name, option_line::find_by_parent_id($qa_cp_element->option_header_id), 'option_line_code', 'option_line_value', $qa_cp_element_value);
-          } else {
-           echo $f->text_field($qa_cp_element->sys_element_name, $qa_cp_element_value);
-          }
-          break;
-         default :
-          echo $f->text_field($qa_cp_element->sys_element_name, $qa_cp_element_value);
-          break;
-        }
-
-        echo '</li>';
-       }
-       ?>
-      </ul>
-     </div>
-    </div> 
-   </div>
-  </div>
- </div> 
-</form>
-
+ </form>
+</div>
 <div id="js_data">
  <ul id="js_saving_data">
   <li class="headerClassName" data-headerClassName="gl_budget" ></li>
@@ -101,7 +84,6 @@
   <li class="primary_column_id" data-primary_column_id="gl_budget_id" ></li>
   <li class="form_header_id" data-form_header_id="gl_budget" ></li>
  </ul>
-
  <ul id="js_contextMenu_data">
   <li class="docHedaderId" data-docHedaderId="gl_budget_id" ></li>
   <li class="btn1DivId" data-btn1DivId="gl_budget" ></li>
