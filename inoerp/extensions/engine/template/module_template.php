06c74c73c2a25181bf163185e37f8be780b556fc
inoerp <contact@inoideas.org>
2016-11-08 21:26:33 +0000

    0.6.1

---
+++ inoerp/extensions/engine/template/module_template.php
@@ -0,0 +1,27 @@
+<?php
+
+$all_modules = option_header::modules();
+$search_result_statement = "";
+$search_result_statement .= "<table class=\"normal\"><thead><tr>";
+$search_result_statement .= '<th> Module Name </th>';
+$search_result_statement .= '<th> Code </th>';
+$search_result_statement .= '<th> Description </th>';
+$search_result_statement .= '<th> Link </th>';
+$search_result_statement .='</tr></thead>';
+If (!empty($all_modules)) {
+ $search_result_statement .= '<tbody>';
+ foreach ($all_modules as $record) {
+	$dir_path = 'modules'.DS. $record->option_line_code;
+	$link = HOME_URL.'form.php?class_name=engine&mode=9&dir_path='.$dir_path;
+	$search_result_statement .='<tr>';
+	$search_result_statement .='<td>' . $record->option_line_value . '</td>';
+	$search_result_statement .='<td>' . $record->option_line_code . '</td>';
+	$search_result_statement .='<td>' . $record->description . '</td>';
+	$search_result_statement .='<td><a role="button" class="ajax-link btn btn-default" href="' . $link . '">' . $record->option_line_value . '</a></td>';
+	$search_result_statement .='</tr>';
+ }
+ $search_result_statement .='</tbody>';
+}
+
+echo $search_result_statement;
+?>
