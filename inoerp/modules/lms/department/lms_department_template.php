40d3dd56d81b70b7f6d1fb93b29ceb396a709330
inoerp <contact@inoideas.org>
2016-11-14 23:36:16 +0000

    0.6.1

--- inoerp/modules/lms/department/lms_department_template.php
+++ inoerp/modules/lms/department/lms_department_template.php
@@ -1,15 +1,15 @@
 <!-- * 
-inoERP 
+inoERP
  *
- * @copyright   2014 Nishit R. Das
+ * @copyright   2016 Nishit R. Das
  * @license     https://www.mozilla.org/MPL/2.0/
  * @link        http://inoideas.org
  * @source code https://github.com/inoerp/inoERP
 -->
 
 <div id="form_all">
- <span class="heading"><?php echo gettext('Department') ?></span>
- <form method="post" id="lms_department"  name="lms_department" class="lms_department">
+ <span class="heading"><?php echo gettext('Learning Management Departments') ?></span>
+ <form  method="post" id="lms_department"  name="lms_department" class="lms_department">
   <div id ="form_header">
    <div id="tabsHeader">
     <ul class="tabMain">
@@ -19,16 +19,16 @@ inoERP
     </ul>
     <div class="tabContainer"> 
      <div id="tabsHeader-1" class="tabContent">
-       <ul class="column header_field">
-        <li><?php $f->l_text_field_dr_withSearch('lms_department_id'); ?>
-         <a name="show" href="form.php?class_name=lms_department&<?php echo "mode=$mode"; ?>" class="show document_id lms_department_id"><i class="fa fa-refresh"></i></a> 
-        </li>
-        <li><?php $f->l_select_field_from_object('bu_org_id', org::find_all_business(), 'org_id', 'org', $$class->org_id, 'org_id', '', '', $readonly); ?>        </li>
-        <li><?php $f->l_text_field_d('department'); ?></li>
-        <li><?php $f->l_text_field_d('description'); ?></li>
-        <li><?php $f->l_select_field_from_object('department_type', option_header::find_options_byName('LMS_DEPARTMENT_TYPE'), 'option_line_code', 'option_line_value', $$class->department_type, 'department_type', '', '', $readonly); ?>        </li>
-        <li><?php $f->l_status_field_d('status'); ?></li>
-       </ul>
+			<ul class="column header_field">
+			 <li><?php $f->l_text_field_dr_withSearch('lms_department_id'); ?>
+				<a name="show" href="form.php?class_name=lms_department&<?php echo "mode=$mode"; ?>" class="show document_id lms_department_id"><i class="fa fa-refresh"></i></a> 
+			 </li>
+			 <li><?php $f->l_select_field_from_object('org_id', org::find_all_inventory(), 'org_id', 'org', $$class->org_id, 'org_id', '', '', $readonly); ?>        </li>
+			 <li><?php $f->l_text_field_d('department'); ?></li>
+			 <li><?php $f->l_text_field_d('description'); ?></li>
+			 <li><?php $f->l_select_field_from_object('department_type', option_header::find_options_byName('LMS_DEPARTMENT_TYPE'), 'option_line_code', 'option_line_value', $$class->department_type, 'department_type', '', '', $readonly); ?>        </li>
+			 <li><?php $f->l_status_field_d('status'); ?></li>
+			</ul>
      </div>
      <div id="tabsHeader-2" class="tabContent">
       <div> <?php echo ino_attachement($file) ?> </div>
@@ -37,13 +37,13 @@ inoERP
       <div> 
        <div id="comments">
         <div id="comment_list">
-         <?php echo!(empty($comments)) ? $comments : ""; ?>
+						<?php echo!(empty($comments)) ? $comments : ""; ?>
         </div>
         <div id ="display_comment_form">
-         <?php
-         $reference_table = 'lms_department';
-         $reference_id = $$class->lms_department_id;
-         ?>
+						<?php
+						$reference_table = 'lms_department';
+						$reference_id = $$class->lms_department_id;
+						?>
         </div>
         <div id="new_comment">
         </div>
@@ -59,44 +59,45 @@ inoERP
   <div id="tabsLine">
    <ul class="tabMain">
     <li><a href="#tabsLine-1"><?php echo gettext('Resource Assignment') ?></a></li>
+    <li><a href="#tabsLine-2"><?php echo gettext('Overhead Rate - Future') ?> </a></li>
    </ul>
    <div class="tabContainer"> 
-    <form action=""  method="post" id="lms_dept_res_assignment_line"  name="lms_dept_res_assignment_line">
+    <form  method="post" id="lms_dept_res_assignment_line"  name="lms_dept_res_assignment_line">
      <div id="tabsLine-1" class="tabContent">
       <table class="form_line_data_table">
        <thead> 
         <tr>
          <th><?php echo gettext('Action') ?></th>
-         <th><?php echo gettext('Assignment Id') ?>#</th>
+         <th><?php echo gettext('Assignment Id') ?></th>
          <th><?php echo gettext('Cost Type') ?></th>
          <th><?php echo gettext('Resource') ?></th>
-         <th><?php echo gettext('Efficiency') ?>%</th>
-         <th><?php echo gettext('Utilization') ?>%</th>
+         <th><?php echo gettext('Efficiency') ?> %</th>
+         <th><?php echo gettext('Utilization') ?> %</th>
          <th><?php echo gettext('No Of Units') ?></th>
         </tr>
        </thead>
        <tbody class="form_data_line_tbody lms_dept_res_assignment_values" >
-        <?php
-        $count = 0;
-        foreach ($lms_dept_res_assignment_object as $lms_dept_res_assignment) {
-         ?>         
-         <tr class="lms_dept_res_assignment<?php echo $count ?>">
-          <td>
-           <?php
-           echo ino_inline_action($$class_second->lms_dept_res_assignment_id, array('lms_department_id' => $$class->lms_department_id));
-           ?>
-          </td>
-          <td><?php $f->text_field_wid2r('lms_dept_res_assignment_id'); ?></td>
-          <td><?php echo $f->select_field_from_object('cost_type_id', lms_cost_type::find_all(), 'lms_cost_type_id', 'cost_type', $$class_second->cost_type_id, '', 'cost_type_id medium'); ?></td>
-          <td><?php echo $f->select_field_from_object('resource_id', lms_resource::find_all(), 'lms_resource_id', 'resource', $$class_second->resource_id, '', 'resource_id medium'); ?></td>
-          <td><?php form::number_field_wid2('efficiency') ?></td>
-          <td><?php form::number_field_wid2('utilization') ?></td>
-          <td><?php form::number_field_wid2('no_of_units') ?></td>
-         </tr>
-         <?php
-         $count = $count + 1;
-        }
-        ?>
+					 <?php
+					 $count = 0;
+					 foreach ($lms_dept_res_assignment_object as $lms_dept_res_assignment) {
+						?>         
+ 				<tr class="lms_dept_res_assignment<?php echo $count ?>">
+ 				 <td>
+							<?php
+							echo ino_inline_action($$class_second->lms_dept_res_assignment_id, array('lms_department_id' => $$class->lms_department_id));
+							?>
+ 				 </td>
+ 				 <td><?php $f->text_field_wid2r('lms_dept_res_assignment_id', 'always_readonly'); ?></td>
+ 				 <td><?php echo $f->select_field_from_object('cost_type_id', bom_cost_type::find_all(), 'bom_cost_type_id', 'cost_type', $$class_second->cost_type_id, '', 'cost_type_id medium'); ?></td>
+ 				 <td><?php echo $f->select_field_from_object('resource_id', lms_resource::find_all(), 'lms_resource_id', 'resource', $$class_second->resource_id, '', 'resource_id medium'); ?></td>
+ 				 <td><?php form::number_field_wid2('efficiency') ?></td>
+ 				 <td><?php form::number_field_wid2('utilization') ?></td>
+ 				 <td><?php form::number_field_wid2('no_of_units') ?></td>
+ 				</tr>
+				 <?php
+				 $count = $count + 1;
+				}
+				?>
        </tbody>
       </table>
      </div>
--- inoerp/modules/lms/resource/lms_resource_template.php
+++ inoerp/modules/lms/resource/lms_resource_template.php
@@ -8,28 +8,27 @@ inoERP
 -->
 
 <div id ="form_header">
- <form  method="post" id="bom_resource"  name="bom_resource">
-  <span class="heading"><?php echo gettext('Resources') ?></span>
+ <form  method="post" id="lms_resource"  name="lms_resource">
+  <span class="heading"><?php echo gettext('Learning Management Resources') ?></span>
   <div id="tabsHeader">
    <ul class="tabMain">
     <li><a href="#tabsHeader-1"><?php echo gettext('Basic Info') ?></a></li>
     <li><a href="#tabsHeader-2"><?php echo gettext('Costing') ?></a></li>
-    <li><a href="#tabsHeader-3"><?php echo gettext('OSP') ?></a></li>
-    <li><a href="#tabsHeader-4"><?php echo gettext('Employee') ?></a></li>
-    <li><a href="#tabsHeader-5"><?php echo gettext('Equipment') ?></a></li>
-    <li><a href="#tabsHeader-6"><?php echo gettext('Note') ?></a></li>
-    <li><a href="#tabsHeader-7"><?php echo gettext('Attachments') ?></a></li>
+    <li><a href="#tabsHeader-3"><?php echo gettext('Employee') ?></a></li>
+    <li><a href="#tabsHeader-4"><?php echo gettext('Equipment') ?></a></li>
+    <li><a href="#tabsHeader-5"><?php echo gettext('Note') ?></a></li>
+    <li><a href="#tabsHeader-6"><?php echo gettext('Attachments') ?></a></li>
    </ul>
    <div class="tabContainer">
     <div id="tabsHeader-1" class="tabContent">
       <ul class="column header_field">
-       <li><?php $f->l_text_field_dr_withSearch('bom_resource_id'); ?>
-        <a name="show" href="form.php?class_name=bom_resource&<?php echo "mode=$mode"; ?>" class="show document_id bom_resource_id"><i class="fa fa-refresh"></i></a> 
+       <li><?php $f->l_text_field_dr_withSearch('lms_resource_id'); ?>
+        <a name="show" href="form.php?class_name=lms_resource&<?php echo "mode=$mode"; ?>" class="show document_id lms_resource_id"><i class="fa fa-refresh"></i></a> 
        </li>
        <li><?php $f->l_select_field_from_object('org_id', org::find_all_inventory(), 'org_id', 'org', $$class->org_id, 'org_id', '', '', $readonly1); ?>      </li>
        <li><?php $f->l_text_field_d('resource'); ?> </li>
        <li><?php $f->l_text_field_d('description'); ?></li>
-       <li><?php $f->l_select_field_from_object('resource_type', bom_resource::resource_type(), 'option_line_code', 'option_line_value', $$class->resource_type, '', '', 1, $readonly1); ?>       </li>
+       <li><?php $f->l_select_field_from_object('resource_type', lms_resource::resource_type(), 'option_line_code', 'option_line_value', $$class->resource_type, '', '', 1, $readonly1); ?>       </li>
        <li><?php $f->l_select_field_from_object('charge_type', bom_resource::charge_type(), 'option_line_code', 'option_line_value', $$class->charge_type, '', '', 1, $readonly); ?>       </li> 
        <li><?php $f->l_select_field_from_object('uom', uom::find_all(), 'uom_id', 'uom_name', $$class->uom, 'uom', '', 1, $readonly1); ?>       </li>
        <li><?php $f->l_status_field_d('status'); ?></li>
@@ -44,17 +43,10 @@ inoERP
        <li><?php $f->l_checkBox_field_d('standard_rate_cb'); ?>       </li>
       </ul>
     </div>
-
     <div id="tabsHeader-3" class="tabContent">
-      <ul class="column five_column"> 
-       <li><?php $f->l_checkBox_field_d('osp_cb'); ?> </li>
-       <li><label><?php echo gettext('Item Number') ?></label>
-        <?php $f->text_field_wid('osp_item_number', 'select_item_number'); ?>
-        <?php $f->hidden_field_withId('osp_item_id', $$class->osp_item_id); ?>
-        <i class="select_item_number select_popup clickable fa fa-search"></i>
-       </li>
-       <li><?php $f->l_text_field_d('osp_item_description', 'item_description'); ?></li>
-      </ul>
+     <div class="large_shadow_box">
+
+     </div>
     </div>
     <div id="tabsHeader-4" class="tabContent">
      <div class="large_shadow_box">
@@ -62,14 +54,9 @@ inoERP
      </div>
     </div>
     <div id="tabsHeader-5" class="tabContent">
-     <div class="large_shadow_box">
-
-     </div>
-    </div>
-    <div id="tabsHeader-6" class="tabContent">
      <div> <?php echo ino_attachement($file) ?> </div>
     </div>
-    <div id="tabsHeader-7" class="tabContent">
+    <div id="tabsHeader-6" class="tabContent">
      <div> 
       <div id="comments">
        <div id="comment_list">
@@ -77,8 +64,8 @@ inoERP
        </div>
        <div id ="display_comment_form">
         <?php
-        $reference_table = 'bom_resource';
-        $reference_id = $$class->bom_resource_id;
+        $reference_table = 'lms_resource';
+        $reference_id = $$class->lms_resource_id;
         ?>
        </div>
        <div id="new_comment">
@@ -96,7 +83,7 @@ inoERP
 </div>
 
 <div id="form_line" class="form_line"><span class="heading"><?php echo gettext('Resource Cost Lines') ?> </span>
- <form   method="post" id="bom_resource_cost"  name="bom_resource_cost">
+ <form   method="post" id="lms_resource_cost"  name="lms_resource_cost">
   <div id="tabsLine">
    <div class="tabContainer">
     <table class="form_line_data_table">
@@ -113,25 +100,25 @@ inoERP
      <tbody class="form_data_line_tbody">
       <?php
       $count = 0;
-      foreach ($bom_resource_cost_object as $bom_resource_cost) {
-       if (!empty($bom_resource_cost->bom_cost_type)) {
-        $bcy = new bom_cost_type();
-        $bcy_i = $bcy->find_by_keyColumn($bom_resource_cost->bom_cost_type);
-        $bom_resource_cost->bom_cost_type_description = $bcy_i->description;
+      foreach ($lms_resource_cost_object as $lms_resource_cost) {
+       if (!empty($lms_resource_cost->lms_cost_type)) {
+        $bcy = new lms_cost_type();
+        $bcy_i = $bcy->find_by_keyColumn($lms_resource_cost->lms_cost_type);
+        $lms_resource_cost->lms_cost_type_description = $bcy_i->description;
        } else {
-        $bom_resource_cost->bom_cost_type_description = null;
+        $lms_resource_cost->lms_cost_type_description = null;
        }
        ?>         
-       <tr class="bom_resource_cost<?php echo $count ?>">
+       <tr class="lms_resource_cost<?php echo $count ?>">
         <td>
          <?php
-         echo ino_inline_action($$class_second->bom_resource_cost_id, array('bom_resource_id' => $$class->bom_resource_id));
+         echo ino_inline_action($$class_second->lms_resource_cost_id, array('lms_resource_id' => $$class->lms_resource_id));
          ?>
         </td>
         <td><?php $f->seq_field_d($count) ?></td>
-        <td><?php form::text_field_wid2r('bom_resource_cost_id'); ?></td>
-        <td><?php echo $f->select_field_from_object('bom_cost_type', bom_cost_type::find_all(), 'cost_type_code', 'cost_type', $$class_second->bom_cost_type, '', 'medium', 1, $readonly); ?></td>
-        <td><?php $f->text_field_wid2r('bom_cost_type_description' , 'xlarge'); ?></td>
+        <td><?php form::text_field_wid2r('lms_resource_cost_id'); ?></td>
+        <td><?php echo $f->select_field_from_object('lms_cost_type', bom_cost_type::find_all(), 'cost_type_code', 'cost_type', $$class_second->lms_cost_type, '', 'medium', 1, $readonly); ?></td>
+        <td><?php $f->text_field_wid2r('lms_cost_type_description' , 'xlarge'); ?></td>
         <td><?php echo $f->number_field('resource_rate', $$class_second->resource_rate); ?></td>
        </tr>
        <?php
@@ -147,21 +134,21 @@ inoERP
 
 <div id="js_data">
  <ul id="js_saving_data">
-  <li class="headerClassName" data-headerClassName="bom_resource" ></li>
-  <li class="lineClassName" data-lineClassName="bom_resource_cost" ></li>
+  <li class="headerClassName" data-headerClassName="lms_resource" ></li>
+  <li class="lineClassName" data-lineClassName="lms_resource_cost" ></li>
   <li class="savingOnlyHeader" data-savingOnlyHeader="false" ></li>
-  <li class="primary_column_id" data-primary_column_id="bom_resource_id" ></li>
-  <li class="form_header_id" data-form_header_id="bom_resource" ></li>
-  <li class="line_key_field" data-line_key_field="bom_cost_type" ></li>
+  <li class="primary_column_id" data-primary_column_id="lms_resource_id" ></li>
+  <li class="form_header_id" data-form_header_id="lms_resource" ></li>
+  <li class="line_key_field" data-line_key_field="lms_cost_type" ></li>
   <li class="single_line" data-single_line="true" ></li>
-  <li class="form_line_id" data-form_line_id="bom_resource_cost" ></li>
+  <li class="form_line_id" data-form_line_id="lms_resource_cost" ></li>
  </ul>
  <ul id="js_contextMenu_data">
-  <li class="docHedaderId" data-docHedaderId="bom_resource_id" ></li>
-  <li class="docLineId" data-docLineId="bom_resource_cost_id" ></li>
-  <li class="btn1DivId" data-btn1DivId="bom_resource" ></li>
+  <li class="docHedaderId" data-docHedaderId="lms_resource_id" ></li>
+  <li class="docLineId" data-docLineId="lms_resource_cost_id" ></li>
+  <li class="btn1DivId" data-btn1DivId="lms_resource" ></li>
   <li class="btn2DivId" data-btn2DivId="form_line" ></li>
-  <li class="trClass" data-doctrClass="bom_resource_cost" ></li>
+  <li class="trClass" data-doctrClass="lms_resource_cost" ></li>
   <li class="tbodyClass" data-tbodyClass="form_data_line_tbody" ></li>
   <li class="noOfTabbs" data-noOfTabbs="1" ></li>
  </ul>
