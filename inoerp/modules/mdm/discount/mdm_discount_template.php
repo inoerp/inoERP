e417b5fb9057a63b68b4ba321d32a2d962e0fd86
inoerp <contact@inoideas.org>
2017-03-08 21:45:58 +0000

    0.6.1

--- inoerp/modules/mdm/discount/mdm_discount_template.php
+++ inoerp/modules/mdm/discount/mdm_discount_template.php
@@ -21,10 +21,12 @@ $f->l_text_field_dr_withSearch('mdm_discount_header_id')
        <li><?php $f->l_text_field_d('discount_name'); ?></li>
 			 <li><?php $f->l_text_field_d('discount_code'); ?></li>
 			 
-			 <li><?php $f->l_select_field_from_array('module_name', mdm_price_list_header::$module_a, $$class->module_name, 'module_name', '', '', $readonly); ?></li>
-			 <li><?php $f->l_text_field_d('discount_type'); ?></li>
-			 <li><?php $f->l_text_field_d('discount_percentage'); ?></li>
+			 <li><?php $f->l_select_field_from_array('module_code', mdm_price_list_header::$module_a, $$class->module_code, 'module_code', '', '', $readonly); ?></li>
+			 
+			 <li><?php $f->l_select_field_from_array('discount_type' , mdm_discount_header::$discount_type_a, $$class->discount_type); ?></li>
+			 <li><?php $f->l_number_field('discount_percentage' , $$class->discount_percentage); ?></li>
        <li><?php $f->l_text_field_d('description'); ?></li>
+			 <li><?php $f->l_checkBox_field_d('check_condition_cb'); ?></li>
 			 <li><?php $f->l_status_field_d('status'); ?></li>
       </ul>
      </div>
@@ -80,7 +82,7 @@ $f->l_text_field_dr_withSearch('mdm_discount_header_id')
         <th><?php echo gettext('Field Name') ?></th>
         <th><?php echo gettext('Compare Type') ?></th>
 				<th><?php echo gettext('Value') ?></th>
-				<th><?php echo gettext('Close Bracket') ?></th>
+				<th><?php echo gettext('End Condition') ?></th>
 				<th><?php echo gettext('Line Status') ?></th>
        </tr>
       </thead>
@@ -97,9 +99,9 @@ $f->l_text_field_dr_withSearch('mdm_discount_header_id')
 				<td><?php echo $f->select_field_from_array('condition_type', dbObject::$condition_type_a, $$class_second->condition_type, '', 'medium', 1, $readonly); ?></td>
 				<td><?php $f->text_field_wid2m('element_set'); ?></td>
 				<td><?php $f->text_field_wid2m('element_name'); ?></td>
-				<td><?php echo $f->select_field_from_array('compare_type', dbObject::$control_type_a, $$class_second->compare_type, '' , 'medium', 1, $readonly1); ?></td>
+				<td><?php echo $f->select_field_from_array('compare_type', dbObject::$control_type_a, $$class_second->compare_type, '' , 'medium', 1, $readonly); ?></td>
 				<td><?php $f->text_field_wid2m('compare_value'); ?></td>
-				<td><?php echo $f->select_field_from_array('close_bracket', mdm_discount_line::$bracket_close_a , '', $$class_second->close_bracket); ?></td>
+				<td><?php echo $f->select_field_from_array('close_bracket', mdm_discount_line::$bracket_close_a ,  $$class_second->close_bracket); ?></td>
 				<td><?php echo $f->status_field_d2('status', 'medium'); ?></td>
  			 </tr>
 				<?php
